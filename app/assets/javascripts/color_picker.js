35119f363c6c8fdb90c9dba78cac9b0caa54286a
abhigyan <abhigyan1111902gmail.com>
2018-06-04 12:41:57 +0530

    20X20 grid created

--- app/assets/javascripts/color_picker.js
+++ app/assets/javascripts/color_picker.js
@@ -1,1 +1,12 @@
 $('#color').minicolors();
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+        	boxId = "box" + rows + columns;
+            $("#grid-container").append("<div class='grid' id = boxId></div>");
+        };
+    };
+    $(".grid").width(480/x);
+    $(".grid").height(480/x);
+};
+createGrid(20);
