7728346a0fd816027e0eab02a095333ef74f08f2
abhigyan <abhigyan1111902gmail.com>
2018-06-04 12:24:03 +0530

    added color picker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
         user = User.create(remote_ip: request.remote_ip)
         cookies[:user_name] = user.name
       end
+      @user = User.find_by(name: cookies[:user_name])
 
     end
 end
